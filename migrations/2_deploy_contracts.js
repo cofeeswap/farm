d8ab21f2b710782688d271bfe9e25dffa140bf30
CoffeeMaker <76795902+cofeeswap@users.noreply.github.com>
2021-07-21 19:28:18 +0800

    coffee v2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,3 @@
-const CofeToken = artifacts.require("CofeToken");
-const MasterChef = artifacts.require("MasterChef");
-const SyrupBar = artifacts.require("SyrupBar");
-const Timelock = artifacts.require("Timelock");
-
 module.exports = async function(deployer) {
-
-    var devaddr = "";
-
-    // Deploy Contracts
-
-    await deployer.deploy(CofeToken).then(function() {
-        return deployer.deploy(SyrupBar, CofeToken.address).then(function() {
-            return deployer.deploy(MasterChef, CofeToken.address, SyrupBar.address, devaddr, "1000000000000000000", '4598214');
-        })
-    })
-    
+   
 };
