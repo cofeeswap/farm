32d54b490535649b2e09225c1bfc8eea358ce043
CoffeeMaker <76795902+cofeeswap@users.noreply.github.com>
2021-07-21 19:28:51 +0800

    coffee v2

--- contracts/CofeToken.sol
+++ contracts/CofeToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // CofeToken with Governance.
-contract CofeToken is BEP20('CoffeeSwap Token', 'COFE') {
+contract CofeToken is BEP20('CoffeeSwap Token', 'COFFEE') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
